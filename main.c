5c382c3d6e570bffa80d8a338da86af4c4ab3135
jaca1119 <46866582+jaca1119@users.noreply.github.com>
2019-09-29 17:32:00 +0200

    Fix all code

--- main.c
+++ main.c
@@ -5,184 +5,204 @@
 
 struct student
 {
-	char imie[40];
-	char nazwisko[40];
+	char name[40];
+	char surName[40];
 	int index;
 };
 
 
-int main(int argc, char *argv[]) {	
-	
-	int a;
-	FILE *fp, *pom;
-	
-	
-	printf("1. View students   2.  Add new student\n3. Delete student number: \n4. Exit\nOption number: ");
-		while(scanf("%d" ,&a) == 1)
-		{
-			if (a == 4)
-				break;
-		
-		switch(a)
-		{
-			case 1 : 
-				//wyswietlanie
-				{
-					if((fp = fopen("plik.txt", "rb")) == NULL)
-					{
-						printf("\nEmpty lsit\n");
-					}
-					
-					struct student tmp;
-					int i = 1;
-					while (fread(&tmp, sizeof(struct student), 1, fp) != 0)
-					{
-						printf("%d %s %s %d\n", i, tmp.imie, tmp.nazwisko, tmp.index);
-						i++;
-					}
-					
-					if (i == 1)
-						printf("\nLista jest pusta\n");
-					
-					fclose(fp);
-					
-				}
-				
-				break;
-			
-			case 2 :
-				//dodanie
-				{
-				int nr_ind;
-				
-				struct student Student;
-				
-				printf("\nName: ");
-				scanf("%s", Student.imie);
-				printf("\nSurname: ");
-				scanf("%s", Student.nazwisko);
-				printf("\nIndex number: ");
-				scanf("%d", &Student.index);
+int main(int argc, char *argv[])
+{
+int a;
+FILE *fp, *temporary;
 
-				
-				//debug
-				//printf("%s %s %d", Student.imie, Student.nazwisko, Student.index);
-				
-				if((fp = fopen("plik.txt", "ab")) == NULL)
-				{
-					printf("Error with opening file");
-					exit(1);
-				}
-				
-				fwrite(&Student, sizeof(struct student), 1, fp);
-				
-				fclose(fp);
-			}
-				break;
-			case 3 :
-				//Usuniecie
-				{
-					int ind;
-					struct student tmp;
-				
-					
-					printf("Enter index number of student to delete: ");
-					scanf("%d", &ind);
-					
-					//otwieranie pliku glownego z strukturami
-					if((fp = fopen("plik.txt", "rb")) == NULL)
-					{
-						printf("Error opening file");
-						exit(1);
-					}
-					
-					//tworzenie pliku tymczasowego 
-					if((pom = fopen("temp_student.txt", "wb")) == NULL)
-					{
-						printf("Error opening file");
-						exit(1);
-					}
-					
-					int help = 0;
-					//przepisywanie do pliku temp_student.txt struktur bez studenta o podanym indeksie
-					while (fread(&tmp, sizeof(struct student), 1, fp) != 0)
-					{
-						if (tmp.index != ind)
-						{
-							fwrite(&tmp, sizeof(struct student), 1, pom);
-							help++;	
-						}
-						else
-						{
-							help = -1;
-						}
-					}
-					
-					
-					fclose(fp);
-					fclose(pom);
-					
-					if (help == 0)
-					{
-						printf("\nEmpty list\n");
-						break;
-					}
-					else if (help == -1)
-					{
-						printf("\nStudent deleted\n");
-					}
-					else
-					{
-						printf("\nThe student not found\n");
-						break;
-					}
-					
-					//zamiana trybu otwierania plikow
-					if((fp = fopen("plik.txt", "wb")) == NULL)
-					{
-						printf("Error opening file");
-						exit(1);
-					}
-					
-					//zamiana trybu otwierania plikow
-					if((pom = fopen("temp_student.txt", "rb")) == NULL)
-					{
-						printf("Error opening file");
-						exit(1);
-					}
-					
-					//przepisywanie do glownego pliku
-					while (fread(&tmp, sizeof(struct student), 1, pom) != 0)
-					{
-					
-						fwrite(&tmp, sizeof(struct student), 1, fp);	
-					}
-							
-					fclose(fp);
-					fclose(pom);
-						
-				}
-				break;
-				
-				default :
-					printf("There is no such option. Try again\n");
-					break;
-		
-			
-		}
-		
-		printf("\nOption number: ");
-	
-	}
-	
-	//usuwanie pliku temp_student.txt
-	if(remove("temp_student.txt") == 0)
-	{
-	
-	}
-	
-	fclose(fp);
-	fclose(pom);
-	
-	return 0;
+//helper variables. Should use bool or boolean but before C99 standard for C language there wasn't bool
+int isStudentsCreated = 0;
+int isTemporaryCreated = 0;
+
+printf("1. View students   2.  Add new student\n3. Delete student of index number: \n4. Exit\nOption number: ");
+
+while(scanf("%d" ,&a) == 1)
+{
+    if (a == 4)
+        break;
+
+    switch(a)
+    {
+        case 1 :
+        //displaying
+        {
+            if((fp = fopen("students.txt", "rb")) == NULL)
+            {
+                printf("\nEmpty list\n");
+                isStudentsCreated = 0;
+                break;
+            }
+
+            struct student tmp;
+            int i = 0;
+            while (fread(&tmp, sizeof(struct student), 1, fp) != 0)
+            {
+                printf("%d %s %s %d\n", i + 1, tmp.name, tmp.surName, tmp.index);
+                i++;
+            }
+
+            if (i == 0)
+                printf("\nThe list is empty\n");
+
+            fclose(fp);
+            break;
+        }
+
+        case 2 :
+        //addition
+        {
+            char name[40];
+            char surName[40];
+
+            struct student Student;
+
+            printf("\nName: ");
+            scanf("%s", name);
+            strcpy(Student.name, name);
+            printf("\nSurname: ");
+            scanf("%s", surName);
+            strcpy(Student.surName, surName);
+            printf("\nIndex number: ");
+            scanf("%d", &Student.index);
+
+            //debug
+            //printf("%s %s %d", Student.name, Student.surName, Student.index);
+
+            if((fp = fopen("students.txt", "ab")) == NULL)
+            {
+                printf("Error with opening file");
+                exit(1);
+            }
+
+            fwrite(&Student, sizeof(struct student), 1, fp);
+
+            isStudentsCreated = 1;
+
+            fclose(fp);
+
+            break;
+        }
+
+        case 3 :
+        //deletion
+        {
+            int ind;
+            struct student tmp;
+
+
+            printf("Enter index number of student to delete: ");
+            scanf("%d", &ind);
+
+            //opening main file with structures
+            if((fp = fopen("students.txt", "rb")) == NULL)
+            {
+                printf("Error opening file");
+                exit(1);
+            }
+
+            //creating temporary file
+            if((temporary = fopen("temp_students.txt", "wb")) == NULL)
+            {
+                printf("Error opening file");
+                exit(1);
+            }
+
+            isTemporaryCreated = 1;
+
+            int help = 0;
+
+            //rewriting structures without selected student to temp_students.txt
+            while (fread(&tmp, sizeof(struct student), 1, fp) != 0)
+            {
+                if (tmp.index != ind)
+                {
+                    fwrite(&tmp, sizeof(struct student), 1, temporary);
+                    help++;
+                }
+                else
+                {
+                    help = -1;
+                }
+            }
+
+
+            fclose(fp);
+            fclose(temporary);
+
+            if (help == 0)
+            {
+                printf("\nEmpty list\n");
+                break;
+            }
+            else if (help == -1)
+            {
+                printf("\nStudent deleted\n");
+            }
+            else
+            {
+                printf("\nThe student not found\n");
+                break;
+            }
+
+            //changing files open mode
+            if((fp = fopen("students.txt", "wb")) == NULL)
+            {
+                printf("Error opening file");
+                exit(1);
+            }
+
+            //changing files open mode
+            if((temporary = fopen("temp_students.txt", "rb")) == NULL)
+            {
+                printf("Error opening file");
+                exit(1);
+            }
+
+            //rewriting to main file
+            while (fread(&tmp, sizeof(struct student), 1, temporary) != 0)
+            {
+                fwrite(&tmp, sizeof(struct student), 1, fp);
+            }
+
+            fclose(fp);
+            fclose(temporary);
+
+            break;
+        }
+
+        default :
+            printf("There is no such option. Try again\n");
+            break;
+
+
+    }
+
+    printf("\nOption number: ");
+}
+
+//deleting temporary file temp_students.txt
+if(remove("temp_students.txt") != 0)
+{
+    //error removing file
+}
+
+//if file was not create, use of fclose will give error
+if (isStudentsCreated)
+{
+    fclose(fp);
+}
+
+//if temporary file was not create, use of fclose will give error
+if (isTemporaryCreated)
+{
+    fclose(temporary);
+}
+
+return 0;
 }
